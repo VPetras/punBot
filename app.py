2ea32ec701e3bc1db1fb9edc91e9ccf20612d416
VPetras <vojtechpetrasek@gmail.com>
2022-03-19 00:16:48 +0100

    generating embed with joke

--- app.py
+++ app.py
@@ -19,11 +19,31 @@ import sys
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = "/")
-
-@bot.command("hi")
+bot = commands.Bot(command_prefix = '/')
+"""
+@bot.command('hi')
 async def pun(ctx):
-    await ctx.send("hi")
+    await ctx.send('hi')
+
+@bot.command('h')
+async def help_desc(ctx):
+    await ctx.send('help')
+"""
+
+@bot.command('pun')
+async def pun(ctx):
+    pundog = discord.File('pundog.jpeg')
+    await ctx.send(file=pundog)
+
+@bot.command('gen')
+async def gen_joke(ctx):
+    embed=discord.Embed(title='Z čeho se skládá písek?',
+        description='Z tatrovky',
+        color=discord.Color.blue())
+    img = discord.File('pundog.jpeg')
+    embed.set_image(url='attachment://pundog.jpeg')
+    #embed.add_field(name='Z čeho se skládá písek', value='Z tatrovky', inline=False) 
+    await ctx.send(file=img, embed=embed)
 
 @bot.event
 async def on_ready():
